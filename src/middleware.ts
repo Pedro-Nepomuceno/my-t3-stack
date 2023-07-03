34accf56dab7143aa87a32e3d66bf02257eec4ec
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-07-03 13:49:04 -0700

    signinComponent

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware();
+console.log("running");
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
