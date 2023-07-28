4805995e511f9532915c561923c56bf0639bcc08
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-07-28 13:30:20 -0700

    update profileRouter

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -6,8 +6,33 @@ import { TRPCError } from "@trpc/server";
 
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
-import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
-import { Redis } from "@upstash/redis";
+export const profileRouter = createTRPCRouter({
+  getUserByUsername: publicProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ input }) => {
+      const [user] = await clerkClient.users.getUserList({
+        username: [input.username],
+      });
 
-export const profileRouter = createTRPCRouter({});
+      if (!user) {
+        // if we hit here we need a unsantized username so hit api once more and find the user.
+        const users = await clerkClient.users.getUserList({
+          limit: 200,
+        });
+        const user = users.find((user) =>
+          user.externalAccounts.find(
+            (account) => account.username === input.username
+          )
+        );
+        if (!user) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "User not found",
+          });
+        }
+        return filterUserForClient(user);
+      }
+    }),
+});
